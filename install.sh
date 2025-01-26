e7bda87f33475e5cd07c89ed8b38c62c9fe0847c
JustAGuyLinux <jag@justaguylinux.com>
2025-01-26 18:40:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -106,28 +106,28 @@ mkdir -p ~/Screenshots/
 # Run theming scripts using source
 if [ -f ./theming/picom.sh ]; then
     echo "Running picom.sh..."
-    source ./theming/picom.sh
+    source ~/.config/bspwm/theming/picom.sh
 else
     echo "picom.sh not found!"
 fi
 
 if [ -f ./theming/nerdfonts.sh ]; then
     echo "Running nerdfonts.sh..."
-    source ./theming/nerdfonts.sh
+    source ~/.config/bspwm/theming/nerdfonts.sh
 else
     echo "nerdfonts.sh not found!"
 fi
 
 if [ -f ./theming/teal.sh ]; then
     echo "Running teal.sh..."
-    source ./theming/teal.sh
+    source ~/.config/bspwm/theming/teal.sh
 else
     echo "teal.sh not found!"
 fi
 
 if [ -f ./theming/update-gtk.sh ]; then
     echo "Running update-gtk.sh..."
-    source ./theming/update-gtk.sh
+    source ~/.config/bspwm/theming/update-gtk.sh
 else
     echo "update-gtk.sh not found!"
 fi
@@ -135,7 +135,7 @@ fi
 # Source the Ghostty install script from the theming directory
 if [ -f ./theming/ghostty.sh ]; then
     echo "Running install_ghostty.sh..."
-    source ./theming/ghostty.sh
+    source ~/.config/bspwm/theming/ghostty.sh
 else
     echo "ghostty.sh not found in the theming directory!"
 fi
